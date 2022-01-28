5496c8e6ef26aa6c898367c4e87ea317f3da98c8
EvgeniyReut <mevmrefman@gmail.com>
2022-01-28 16:26:34 +0300

    add: js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 module.exports = function reverse (n) {
-    let result=Number(String(Math.abs(n)).split('').reverse().join(''))
-    return result;
+    return Number(String(Math.abs(n)).split('').reverse().join(''));
 }
